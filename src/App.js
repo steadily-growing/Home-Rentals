1ba75cd4ada28c99c40fe42c655504a9cb054870
abena07 <abenabennett@gmail.com>
2023-01-15 23:56:31 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import nav from './components/nav'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auctions from './pages/auctions'
+import Drop from './pages/drop'
+import Home from './pages/home'
+import Marketplace from './pages/marketplace'
+import  NoPage from './pages/NoPage'
 
 function App() {
   return (
     <div className="App">
-    <div>hello world</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+          <Route path="auctions" element={<Auctions />} />
+          <Route path="drop" element={<Drop />} />
+          <Route path="market" element={<Marketplace />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
 
 function nav() {
-  return (
-    <div>nav</div>
-  )                                                                                        
+
+    <>
+            <div className='p-4'>
+                <ul className='flex flex-row  w-full justify-center'>
+                    <div className='flex flex-row justify-evenly '>
+                    <div className='pr-32 text-2xl'>
+                        <li>
+                            <Link to ="/">ARTSY.</Link>
+                        </li>
+                    </div>
+                    <div className=' hover:underline px-12' >
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </div>
+                    <div className='hover:underline px-12'>
+                        <li>
+                            <Link to="/auctions">Auction</Link>
+                        </li>
+                    </div>
+                    <div className='hover:underline px-12 '>
+                        <li>
+                            <Link to="/market">Market</Link>
+                        </li>
+                    </div>
+                    <div className=' hover:underline px-12 '>
+                        <li>
+                            <Link to="/drop">Drop</Link>
+                        </li>
+                    </div>
+                    
+                    </div>
+
+                    <div className='flex flex-row justify-end '>
+                
+                    </div>
+                </ul>
+
+            </div>
+
+            <Outlet />
+        </>
+                                                                                   
 }
 
 export default nav
