99a164a2a4388449b58d660cf5948081a9291337
abena07 <abenabennett@gmail.com>
2023-01-20 16:58:02 +0000

    added bg image

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,36 +2,37 @@ import React from 'react'
 import { Outlet, Link } from "react-router-dom";
 import logo from '../assets/logo 1.png'
 
+
 function Nav() {
 return(
     <>
-            <div className='p-4'>
-                <ul className='flex flex-row  w-full justify-center'>
+            <div className='p-4 absolute'>
+                <ul className='flex flex-row w-full justify-center'>
                     <div className='flex flex-row justify-evenly '>
                     <div className='pr-32 text-2xl'>
                         <li>
                             <Link to ="/"><img src={logo}/></Link>
                         </li>
                     </div>
-                    <div className=' hover:underline px-12' >
+                    <div className='px-12' >
                         
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/"><p className='text-white'>Home</p></Link>
                         </li>
                     </div>
-                    <div className='hover:underline px-12'>
+                    <div className='px-12'>
                         <li>
-                            <Link to="/landlord">Landlord</Link>
+                            <Link to="/landlord" ><p className='text-white'>Landlord</p></Link>
                         </li>
                     </div>
-                    <div className='hover:underline px-12 '>
+                    <div className=' px-12 '>
                         <li>
-                            <Link to="/tenants">Tenants</Link>
+                            <Link to="/tenants"><p className='text-white'>Tenants</p></Link>
                         </li>
                     </div>
-                    <div className=' hover:underline px-12 '>
+                    <div className=' px-12 '>
                         <li>
-                            <Link to="/contactus">Contact Us</Link>
+                            <Link to="/contactus"><p className='text-white'>Contact Us</p></Link>
                         </li>
                     </div>
                     
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,9 +4,9 @@ import bg from '../assets/bg.png'
 
 function home() {
   return (
-    <div className=''>
+    <div className='relative'>
     <img src ={bg}/>
-   
+    <p className='absolute left-0 top-48 text-4xl text-white font-bold'>The most affordable <br/>place to stay in the<br/> san franciso bay area</p>
     </div>
   
   )
