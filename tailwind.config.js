45e917c3bdd3d8c8d9769b97e91fca90cc3a0d7b
abena07 <abenabennett@gmail.com>
2023-01-20 01:42:20 +0000

    home page in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero':"url(C:\Users\abena\OneDrive\Desktop\frontend\home_rentals\src\assets\bg.png)"
+      }
+    },
   },
   plugins: [],
 }
