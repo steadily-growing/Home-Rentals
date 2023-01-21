81246971d291b04e2b90089a628a178607e07de6
abena07 <abenabennett@gmail.com>
2023-01-21 04:05:08 +0000

    mininum living cost section in progress

---
+++ src/components/homeSection2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import glass from '../assets/glass.png'
+import dollar from '../assets/dollar.svg'
+
+function homeSection2() {
+  return (
+    <div>
+      <p className='text-3xl font-[Inter_ExtraBold] font-bold pt-14 pb-2'>Minimum Living Cost Takes Care Of Everything</p>
+      <hr className="border-2 border-orange-600 w-80 " />
+      <img src={glass}/>
+    
+    
+    </div>
+  )
+}
+
+export default homeSection2
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import bg from '../assets/bg.png'
 import map from '../assets/map.png'
 import search from '../assets/search.png'
+import Home from '../components/homeSection2'
 
 
 function home() {
@@ -9,8 +10,9 @@ function home() {
     <div className='relative'>
     <img src ={bg}/>
     <p className='absolute left-0 top-48 text-5xl text-white font-bold pl-10 pt-10 font-[Inter]'>The Most Affordable <br/>Place To Stay In The<br/> San Franciso Bay Area</p>
-    <img src={map} className='absolute left-96 top-14 pl-64 '/>
+    <img src={map} className='absolute left-96 top-14 pl-60'/>
     <img src={search} className='absolute left-96 top-96 pl-64 pt-12'/>
+    <Home/>
     </div>
   
   )
