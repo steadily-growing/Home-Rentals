0231c7f2e70e0971b8b81983697f5b3f0b8c06aa
abena07 <abenabennett@gmail.com>
2023-03-08 21:23:04 +0000

    99% done w the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,21 +14,23 @@ import wifi from "../assets/wifi.png";
 function footer() {
   return (
     <div className="pr-24 pt-24 pb-10 ml-28">
+      <div class="border-t border-orange-400 h-0 pb-8"></div>
+
       <div className=" flex flex-col ">
         <div className="flex justify-between">
           {/* logo */}
-          <div className="">logo</div>
+          <div className="font-bold text-4xl">Logo</div>
           {/* details */}
-          <div className="flex">
+          <div className="flex p-2">
             {/* first section :location phone and printer  */}
             <div className="flex flex-col">
               {/* location */}
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between p-2">
                 <img src={landmark} />
                 <p>345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345</p>
               </div>
               {/* phone and printer */}
-              <div className="flex justify-between">
+              <div className="flex justify-between p-2">
                 <div className="flex justify-between  ">
                   <img src={phone} />
                   <p>(123) 456-7890</p>
@@ -40,7 +42,7 @@ function footer() {
                 </div>
               </div>
               {/* social media */}
-              <div className="flex items-center justify-between w-[60%] ">
+              <div className="flex items-center justify-between p-2 ">
                 <div>Social Media</div>
                 <img src={fbk} />
                 <img src={twt} />
@@ -56,8 +58,10 @@ function footer() {
         </div>
       </div>
       {/* text */}
+      <div class="border-t border-gray-200 h-0 pb-4"></div>
       <div className="flex justify-between ">
-        <div className="flex justify-evenly">
+        
+        <div className="flex space-between justify-between">
           <div>ABOUT US </div>
           <div>CONTACT US</div>
           <div>HELP</div>
