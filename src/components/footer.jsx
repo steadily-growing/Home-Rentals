40a5ae661687249c09c419969c8350e8e5775321
abena07 <abenabennett@gmail.com>
2023-03-08 20:58:37 +0000

    80% done w the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,63 +13,62 @@ import wifi from "../assets/wifi.png";
 
 function footer() {
   return (
-    <div className=" flex flex-col  px-[10%] py-[5%] w-full" >
-      <div className="flex">
+    <div className="pr-24 pt-24 pb-10 ml-28">
+      <div className=" flex flex-col ">
+        <div className="flex justify-between">
           {/* logo */}
-      <div>logo</div>
-      {/* details */}
-      <div className="">
-        {/* first section :location phone and printer  */}
-        <div className="flex flex-col items-center justify-between w-[65%]">
-          {/* location */}
+          <div className="">logo</div>
+          {/* details */}
           <div className="flex">
-            <img src={landmark} />
-            <p>345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345</p>
-          </div>
-          {/* phone and printer */}
-          <div className="flex">
-            <div className="flex">
-              <img src={phone} />
-              <p>(123) 456-7890</p>
-            </div>
-            {/* printer */}
-            <div className="flex">
-              <img src={printer} />
-              <p>(123) 456-7890</p>
+            {/* first section :location phone and printer  */}
+            <div className="flex flex-col">
+              {/* location */}
+              <div className="flex items-center justify-between">
+                <img src={landmark} />
+                <p>345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345</p>
+              </div>
+              {/* phone and printer */}
+              <div className="flex justify-between">
+                <div className="flex justify-between  ">
+                  <img src={phone} />
+                  <p>(123) 456-7890</p>
+                </div>
+                {/* printer */}
+                <div className="flex">
+                  <img src={printer} />
+                  <p>(123) 456-7890</p>
+                </div>
+              </div>
+              {/* social media */}
+              <div className="flex items-center justify-between w-[60%] ">
+                <div>Social Media</div>
+                <img src={fbk} />
+                <img src={twt} />
+                <img src={lnkdn} />
+                <img src={yutube} />
+                <img src={ig} />
+                <img src={google} />
+                <img src={pinterest} />
+                <img src={wifi} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    
-      {/* social media */}
-      <div className="flex items-center justify-between w-[65%]">
-        <div>Social Media</div>
-        <img src={fbk}/>
-        <img src={twt}/>
-        <img src={lnkdn}/>
-        <img src={yutube}/>
-        <img src={ig}/>
-        <img src={google}/>
-        <img src={pinterest}/>
-        <img src={wifi}/>
-      </div>
       {/* text */}
-      <div className="flex">
-        <div>ABOUT US</div>
-        <div>CONTACT US</div>
-        <div>HELP</div>
-        <div>PRIVACY POLICY</div>
-        <div>DISCLAIMER</div>
-
-      </div>
-      {/* copyright */}
-      <div>
-      Copyright © 2020 Minimumlivingcost. All rights reserved
+      <div className="flex justify-between ">
+        <div className="flex justify-evenly">
+          <div>ABOUT US </div>
+          <div>CONTACT US</div>
+          <div>HELP</div>
+          <div>PRIVACY POLICY</div>
+          <div>DISCLAIMER</div>
+        </div>
+        {/* copyright */}
+        <div>Copyright © 2020 Minimumlivingcost. All rights reserved</div>
       </div>
     </div>
   );
-
 }
 
 export default footer;
